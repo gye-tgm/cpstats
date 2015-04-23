f767a8cd74798655ff60f25d4b45a69be415d74a
Gary Ye <gye@student.tgm.ac.at>
2015-04-23 22:18:49 +0200

    split user profile to files

--- cpstats/view.py
+++ cpstats/view.py
@@ -7,13 +7,37 @@ from cpstats import app
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/user/<uname>')
-def user_profile(uname):
+SUBNAV = [
+    'general',
+    'submissions',
+    'tasks',
+    'achievements'
+]
+
+def user_accounts(username):
     accounts = []
     session = model.Session()
-    user = session.query(model.User).filter_by(uname=uname).one()
+    user = session.query(model.User).filter_by(uname=username).one()
     for acc in user.accounts:
         accounts.append({'handle': acc.handle, 'url': acc.url, 'oj': acc.oj_id})
-    return render_template('user.html',
-                           username=uname,
-                           accounts=accounts)
+    return accounts
+
+@app.route('/user/<string:username>')
+@app.route('/user/<string:username>/general')
+def user_general(username):
+    return render_template('user_general.html',
+                           username=username,
+                           accounts=user_accounts(username),
+                           title='general',
+                           subnav=SUBNAV)
+
+@app.route('/user/<string:username>/tasks')
+def user_tasks(username):
+    # todo: get the tasks man!
+    tasks = None
+    return render_template('user_tasks.html',
+                           username=username,
+                           accounts=user_accounts(username),
+                           tasks=tasks,
+                           title='tasks',
+                           subnav=SUBNAV)
