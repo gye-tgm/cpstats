4420d6e64ff4e08a7316d1ed1cdb48c633cf1b26
Gary Ye <gye@student.tgm.ac.at>
2015-04-23 20:24:43 +0200

    added first prototype

--- cpstats/view.py
+++ cpstats/view.py
@@ -1,7 +1,19 @@
 from flask import render_template
+from cpstats.models import model
 from cpstats import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/user/<uname>')
+def user_profile(uname):
+    accounts = []
+    session = model.Session()
+    user = session.query(model.User).filter_by(uname=uname).one()
+    for acc in user.accounts:
+        accounts.append({'handle': acc.handle, 'url': acc.url, 'oj': acc.oj_id})
+    return render_template('user.html',
+                           username=uname,
+                           accounts=accounts)
