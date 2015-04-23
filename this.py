1b97e4dd0b0a5547dab9d5d64c4bcf4b914ca654
Gary Ye <gye@student.tgm.ac.at>
2015-04-23 23:32:01 +0200

    Added test file

---
+++ this.py
@@ -0,0 +1,16 @@
+from cpstats.models.model import *
+from cpstats.crawl.codeforces import * 
+
+session = Session()
+
+gdis_cf = CodeforcesAccount(handle='gdisastery', url='http://codeforces.com/profile/gdisastery', rating=2059, contribution=12)
+
+session.add(gdis_cf)
+session.commit()
+
+print("Account created")
+c = CodeforcesCrawler()
+c.crawl_tasks()
+print("Tasks crawled")
+c.crawl_submissions('gdisastery')
+print("Submissions of gdisastery crawled")
